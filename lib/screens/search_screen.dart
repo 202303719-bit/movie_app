8f36ed5c109b9b124840ab496a001ea8ba35c577
reham helmy aboelnaga <rehamhelmy987@gmail.com>
2026-09-08 11:47:53 +0300

    Add files via upload

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../blocs/search/search_bloc.dart';
+import '../blocs/search/search_event.dart';
+import '../blocs/search/search_state.dart';
+import '../models/movie.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => SearchBloc(),
+      child: const _SearchView(),
+    );
+  }
+}
+
+class _SearchView extends StatefulWidget {
+  const _SearchView();
+
+  @override
+  State<_SearchView> createState() => _SearchViewState();
+}
+
+class _SearchViewState extends State<_SearchView> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _search() {
+    context.read<SearchBloc>().add(
+      SearchMovies(_searchController.text),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              TextField(
+                controller: _searchController,
+                textInputAction: TextInputAction.search,
+                onSubmitted: (_) => _search(),
+                decoration: InputDecoration(
+                  hintText: 'Search for a movie...',
+                  prefixIcon: const Icon(Icons.search),
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.clear),
+                    onPressed: () {
+                      _searchController.clear();
+                      context.read<SearchBloc>().add(
+                        const SearchMovies(''),
+                      );
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Expanded(
+                child: BlocBuilder<SearchBloc, SearchState>(
+                  builder: (context, state) {
+                    if (state is SearchInitial) {
+                      return const Center(
+                        child: Text('Search for your favorite movies'),
+                      );
+                    }
+
+                    if (state is SearchLoading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+
+                    if (state is SearchEmpty) {
+                      return const Center(
+                        child: Text('No movies found'),
+                      );
+                    }
+
+                    if (state is SearchError) {
+                      return Center(
+                        child: Text(
+                          'Something went wrong\n${state.message}',
+                          textAlign: TextAlign.center,
+                        ),
+                      );
+                    }
+
+                    if (state is SearchSuccess) {
+                      return _MovieGrid(movies: state.movies);
+                    }
+
+                    return const SizedBox.shrink();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _MovieGrid extends StatelessWidget {
+  final List<Movie> movies;
+
+  const _MovieGrid({
+    required this.movies,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.only(bottom: 16),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 180,
+        childAspectRatio: 0.62,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 16,
+      ),
+      itemCount: movies.length,
+      itemBuilder: (context, index) {
+        final movie = movies[index];
+
+        return _MovieCard(movie: movie);
+      },
+    );
+  }
+}
+
+class _MovieCard extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieCard({
+    required this.movie,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: movie.mediumCoverImage.isNotEmpty
+                ? Image.network(
+              movie.mediumCoverImage,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              errorBuilder: (_, __, ___) {
+                return const Center(
+                  child: Icon(Icons.movie),
+                );
+              },
+            )
+                : const Center(
+              child: Icon(Icons.movie),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 6),
+
+        Text(
+          movie.title,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+
+        const SizedBox(height: 3),
+
+        Text(
+          '${movie.year} • ⭐ ${movie.rating}',
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
+}
