1b34d577d71300daaaeaaf2b430f011e4654365b
Ba7r-a <ahmedemba48@gmail.com>
2026-09-10 18:57:30 +0300

    Delete lib/core directory

--- lib/core/constants/app_constants.dart
+++
@@ -1,5 +0,0 @@
-class AppConstants {
-  static const String apiKey = '8d34aff24549adbdbe9baf374d04de19';
-  static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
-}
